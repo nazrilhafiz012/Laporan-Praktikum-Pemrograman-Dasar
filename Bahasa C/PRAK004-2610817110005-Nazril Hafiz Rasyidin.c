ee371117143d5ebadc27f981c579bbef34e4e95a
nazrilhafiz012 <nazrilhafiz0@gmail.com>
2026-09-18 11:53:14 +0800

    Add files via upload

---
+++ Bahasa C/PRAK004-2610817110005-Nazril Hafiz Rasyidin.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+    printf("################################\n");
+    printf("#                              #\n");
+    printf("#    Nazril Hafiz Rasyidin     #\n");
+    printf("#        2610817110005         #\n");
+    printf("#                              #\n");
+    printf("################################\n");
+    return 0;
+}
---
+++ Bahasa Python/PRAK002-2610817110005-Nazril Hafiz Rasyidin.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi, Nazril Hafiz Rasyidin")
+print("Selamat Siang, Nazril Hafiz Rasyidin")
+print("Selamat Malam, Nazril Hafiz Rasyidin")
